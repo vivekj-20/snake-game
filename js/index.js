13f14b237cfe86d3a0cab043fedfcd60b838b332
Vivek Jena <vivekjena.20@gmail.com>
2022-07-02 01:23:42 +0530

    Version 3

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 // Game Constants & Variables
 let inputDir = {x: 0, y: 0}; 
-const foodSound = new Audio('music/food.mp3');
-const gameOverSound = new Audio('music/gameover.mp3');
-const moveSound = new Audio('music/move.mp3');
-const musicSound = new Audio('music/music.mp3');
-let speed = 19;
+const foodSound = new Audio('food.mp3');
+const gameOverSound = new Audio('gameover.mp3');
+const moveSound = new Audio('move.mp3');
+const musicSound = new Audio('music.mp3');
+let speed = 10;
 let score = 0;
 let lastPaintTime = 0;
 let snakeArr = [
@@ -12,8 +12,6 @@ let snakeArr = [
 ];
 
 food = {x: 6, y: 7};
-
-//VERSION 2.0
 
 // Game Functions
 function main(ctime) {
